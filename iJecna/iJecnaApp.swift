403f35b645f94249dd05220350730e4124585ebe
vanya <mitrofanovvana20082@gmail.com>
2026-09-03 09:26:06 +0200

    Initial Commit

---
+++ iJecna/iJecnaApp.swift
@@ -0,0 +1,17 @@
+//
+//  iJecnaApp.swift
+//  iJecna
+//
+//  Created by Vanya on 03.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct iJecnaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
